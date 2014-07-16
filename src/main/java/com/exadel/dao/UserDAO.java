767d149be454b630392f2612231a369c394856f4
IPoyda <Rock.Ivan.Poyda@gmail.com>
2014-07-16 18:37:11 +0300

    added hibernate User and Student, lots of xml and daos

---
+++ src/main/java/com/exadel/dao/UserDAO.java
@@ -0,0 +1,17 @@
+package com.exadel.dao;
+
+
+import logic.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface UserDAO {
+
+    public void addUser(User user) throws SQLException;   //добавить студента
+    public void updateUser(User user) throws SQLException;//обновить студента
+    public User getUserById(Long id) throws SQLException;    //получить стедента по id
+    public List getAllUsers() throws SQLException;              //получить всех студентов
+    public void deleteUser(User user) throws SQLException;//удалить студента
+
+}
---
+++ src/main/java/com/exadel/util/HibernateUtil.java
@@ -0,0 +1,23 @@
+package com.exadel.util;
+
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory = null;
+
+    static {
+        try {
+            //creates the session factory from hibernate.cfg.xml
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
