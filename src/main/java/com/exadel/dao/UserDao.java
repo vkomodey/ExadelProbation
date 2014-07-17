2e7729460f30e82cb389c6e5841856b00669b056
garethspurs <garethspurs95@gmail.com>
2014-07-17 20:46:39 +0300

    Need some DAO?

---
+++ src/main/java/com/exadel/dao/UserDao.java
@@ -0,0 +1,7 @@
+package com.exadel.dao;
+
+/**
+ * Created by vlad on 17/07/14.
+ */
+public class UserDao {
+}
---
+++ src/main/java/com/exadel/dao/impl/UserDaoImpl.java
@@ -0,0 +1,7 @@
+package com.exadel.dao.impl;
+
+/**
+ * Created by vlad on 17/07/14.
+ */
+public class UserDaoImpl {
+}
