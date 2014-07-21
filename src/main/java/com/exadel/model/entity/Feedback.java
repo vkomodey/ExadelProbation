b72b30e5c2001112c75f724a533fe40f93ed978b
IPoyda <Rock.Ivan.Poyda@gmail.com>
2014-07-21 14:00:31 +0300

    FeedBack is cool

--- src/main/java/com/exadel/model/entity/Feedback.java
+++ src/main/java/com/exadel/model/entity/Feedback.java
@@ -8,31 +8,35 @@ import java.util.Calendar;
 @Entity
 @Table(name = "feedback")
 public class Feedback {
-    private long student_id,id;
-    private boolean prof_competence,need_more_hours;
-    private String work_attitude,collective_relations,prof_matters_progress;
+    private long studentId;
+    private long id;
+    private boolean profCompetence;
+    private boolean needMoreHours;
+    private String workAttitude;
+    private String collectiveRelations;
+    private String profMattersProgress;
     private String feedback;
-    private Calendar feedback_date;
+    private Calendar feedbackDate;
 
     public Feedback() {
     }
 
-    @Column(name = "feedback_date")
-    public Calendar getFeedback_date() {
-        return feedback_date;
+    @Column(name = "feedbackDate")
+    public Calendar getFeedbackDate() {
+        return feedbackDate;
     }
 
-    public void setFeedback_date(Calendar feedback_date) {
-        this.feedback_date = feedback_date;
+    public void setFeedbackDate(Calendar feedbackDate) {
+        this.feedbackDate = feedbackDate;
     }
 
     @Column(name = "stud_id")
-    public long getStudent_id() {
-        return student_id;
+    public long getStudentId() {
+        return studentId;
     }
 
-    public void setStudent_id(long student_id) {
-        this.student_id = student_id;
+    public void setStudentId(long studentId) {
+        this.studentId = studentId;
     }
 
     @Column(name = "id")
@@ -44,49 +48,49 @@ public class Feedback {
         this.id = id;
     }
 
-    @Column(name = "prof_competence")
-    public boolean isProf_competence() {
-        return prof_competence;
+    @Column(name = "profCompetence")
+    public boolean isProfCompetence() {
+        return profCompetence;
     }
 
-    public void setProf_competence(boolean prof_competence) {
-        this.prof_competence = prof_competence;
+    public void setProfCompetence(boolean profCompetence) {
+        this.profCompetence = profCompetence;
     }
 
-    @Column(name = "need_more_hours")
-    public boolean isNeed_more_hours() {
-        return need_more_hours;
+    @Column(name = "needMoreHours")
+    public boolean isNeedMoreHours() {
+        return needMoreHours;
     }
 
-    public void setNeed_more_hours(boolean need_more_hours) {
-        this.need_more_hours = need_more_hours;
+    public void setNeedMoreHours(boolean needMoreHours) {
+        this.needMoreHours = needMoreHours;
     }
 
-    @Column(name = "work_attitude")
-    public String getWork_attitude() {
-        return work_attitude;
+    @Column(name = "workAttitude")
+    public String getWorkAttitude() {
+        return workAttitude;
     }
 
-    public void setWork_attitude(String work_attitude) {
-        this.work_attitude = work_attitude;
+    public void setWorkAttitude(String workAttitude) {
+        this.workAttitude = workAttitude;
     }
 
-    @Column(name = "collective_relations")
-    public String getCollective_relations() {
-        return collective_relations;
+    @Column(name = "collectiveRelations")
+    public String getCollectiveRelations() {
+        return collectiveRelations;
     }
 
-    public void setCollective_relations(String collective_relations) {
-        this.collective_relations = collective_relations;
+    public void setCollectiveRelations(String collectiveRelations) {
+        this.collectiveRelations = collectiveRelations;
     }
 
-    @Column(name = "prof_matters_progress")
-    public String getProf_matters_progress() {
-        return prof_matters_progress;
+    @Column(name = "profMattersProgress")
+    public String getProfMattersProgress() {
+        return profMattersProgress;
     }
 
-    public void setProf_matters_progress(String prof_matters_progress) {
-        this.prof_matters_progress = prof_matters_progress;
+    public void setProfMattersProgress(String profMattersProgress) {
+        this.profMattersProgress = profMattersProgress;
     }
 
     @Column(name = "feedback")
