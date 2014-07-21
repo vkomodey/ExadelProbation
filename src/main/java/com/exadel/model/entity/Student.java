e1e9d151392ca089965f1a0348e6f40142d719a7
Dennis Borisevich <elfmax@tut.by>
2014-07-21 12:10:44 +0300

    finally merged work and practice

--- src/main/java/com/exadel/model/entity/Student.java
+++ src/main/java/com/exadel/model/entity/Student.java
@@ -3,14 +3,15 @@ package com.exadel.model.entity;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
-
 
 import com.exadel.model.enums.StudentStateEnum;
 
@@ -22,6 +23,9 @@ public class Student extends User{
 	private Study study;
     private StudentStateEnum state;
     private Set<Skill> skillSet;
+    private ExadelPractice practice;
+    private ExadelWork work;
+    
     
     @OneToMany(cascade=CascadeType.ALL,orphanRemoval=true)
     public Set<Skill> getSkillSet() {
@@ -44,6 +48,22 @@ public class Student extends User{
 	}
 	public void setStudy(Study study) {
 		this.study = study;
+	}
+	
+	@OneToOne(cascade=CascadeType.ALL)
+	public ExadelPractice getPractice() {
+		return practice;
+	}
+	public void setPractice(ExadelPractice practice) {
+		this.practice = practice;
+	}
+	
+	@OneToOne(cascade=CascadeType.ALL)
+	public ExadelWork getWork() {
+		return work;
+	}
+	public void setWork(ExadelWork work) {
+		this.work = work;
 	}
 
 
