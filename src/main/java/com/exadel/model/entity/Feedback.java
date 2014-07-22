53894a1ab0d36420927fd0f0c7a662e6b873162e
Dennis Borisevich <elfmax@tut.by>
2014-07-22 16:59:22 +0300

    Feedback added StudentLog fix

--- src/main/java/com/exadel/model/entity/Feedback.java
+++ src/main/java/com/exadel/model/entity/Feedback.java
@@ -2,7 +2,13 @@ package com.exadel.model.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.NaturalId;
+
 import java.util.Calendar;
 
 @Entity
@@ -17,17 +23,26 @@ public class Feedback {
     private String profMattersProgress;
     private String feedback;
     private Calendar feedbackDate;
-
+    /*private User author;*/
+    
     public Feedback() {
-    }
+	}
+/*
+	@ManyToOne
+	@JoinColumn(name="author",referencedColumnName="id")
+    public User getAuthor() {
+		return author;
+	}*/
 
-    @Column(name = "feedbackDate")
+	@Id
+	@Column(name = "id")
+	public long getId() {
+	    return id;
+	}
+
+	@Column(name = "feedbackDate")
     public Calendar getFeedbackDate() {
         return feedbackDate;
-    }
-
-    public void setFeedbackDate(Calendar feedbackDate) {
-        this.feedbackDate = feedbackDate;
     }
 
     @Column(name = "stud_id")
@@ -35,67 +50,69 @@ public class Feedback {
         return studentId;
     }
 
-    public void setStudentId(long studentId) {
+    @Column(name = "profCompetence")
+	public boolean isProfCompetence() {
+	    return profCompetence;
+	}
+
+	@Column(name = "needMoreHours")
+	public boolean isNeedMoreHours() {
+	    return needMoreHours;
+	}
+
+	@Column(name = "workAttitude")
+	public String getWorkAttitude() {
+	    return workAttitude;
+	}
+
+	@Column(name = "collectiveRelations")
+	public String getCollectiveRelations() {
+	    return collectiveRelations;
+	}
+
+	@Column(name = "profMattersProgress")
+	public String getProfMattersProgress() {
+	    return profMattersProgress;
+	}
+
+	@Column(name = "feedback")
+	public String getFeedback() {
+	    return feedback;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public void setFeedbackDate(Calendar feedbackDate) {
+	    this.feedbackDate = feedbackDate;
+	}
+
+/*	public void setAuthor(User author) {
+		this.author = author;
+	}
+*/
+	public void setStudentId(long studentId) {
         this.studentId = studentId;
     }
-
-    @Column(name = "id")
-    public long getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Column(name = "profCompetence")
-    public boolean isProfCompetence() {
-        return profCompetence;
-    }
-
     public void setProfCompetence(boolean profCompetence) {
         this.profCompetence = profCompetence;
-    }
-
-    @Column(name = "needMoreHours")
-    public boolean isNeedMoreHours() {
-        return needMoreHours;
     }
 
     public void setNeedMoreHours(boolean needMoreHours) {
         this.needMoreHours = needMoreHours;
     }
 
-    @Column(name = "workAttitude")
-    public String getWorkAttitude() {
-        return workAttitude;
-    }
-
     public void setWorkAttitude(String workAttitude) {
         this.workAttitude = workAttitude;
-    }
-
-    @Column(name = "collectiveRelations")
-    public String getCollectiveRelations() {
-        return collectiveRelations;
     }
 
     public void setCollectiveRelations(String collectiveRelations) {
         this.collectiveRelations = collectiveRelations;
     }
 
-    @Column(name = "profMattersProgress")
-    public String getProfMattersProgress() {
-        return profMattersProgress;
-    }
-
     public void setProfMattersProgress(String profMattersProgress) {
         this.profMattersProgress = profMattersProgress;
-    }
-
-    @Column(name = "feedback")
-    public String getFeedback() {
-        return feedback;
     }
 
     public void setFeedback(String feedback) {
--- src/main/java/com/exadel/model/entity/Student.java
+++ src/main/java/com/exadel/model/entity/Student.java
@@ -1,5 +1,6 @@
 package com.exadel.model.entity;
 
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -23,16 +24,34 @@ public class Student extends User{
     private Set<Skill> skillSet;
     private ExadelPractice practice;
     private ExadelWork work;
+    private List<Feedback> feedback;
+    @OneToMany(cascade=CascadeType.ALL)
+    @JoinColumn(referencedColumnName="id")
+    public List<Feedback> getFeedback() {
+		return feedback;
+	}
+	public void setFeedback(List<Feedback> feedback) {
+		this.feedback = feedback;
+	}
+	private String email;
+    private String skype;
+    private String phone;
     
-    
-    @OneToMany(cascade=CascadeType.ALL,orphanRemoval=true)
+    public String getEmail() {
+		return email;
+	}
+	public String getSkype() {
+		return skype;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	
+	@OneToMany(cascade=CascadeType.ALL,orphanRemoval=true)
     public Set<Skill> getSkillSet() {
 		return skillSet;
 	}
-	public void setSkillSet(Set<Skill> skillSet) {
-		this.skillSet = skillSet;
-	}
-    @Enumerated(EnumType.STRING)
+	@Enumerated(EnumType.STRING)
 	public StudentStateEnum getState() {
 		return state;
 	}
@@ -44,29 +63,39 @@ public class Student extends User{
 	public Study getStudy() {
 		return study;
 	}
-	public void setStudy(Study study) {
-		this.study = study;
-	}
-	
 	@OneToOne(cascade = CascadeType.ALL, optional=true,orphanRemoval=true)
 	@JoinColumn (name="id", nullable=false)
 	public ExadelPractice getPractice() {
 		return practice;
+	}
+	@OneToOne(cascade = CascadeType.ALL, optional=true,orphanRemoval=true)
+	@JoinColumn (name="id", nullable=false)
+	public ExadelWork getWork() {
+		return work;
+	}
+	public void setStudy(Study study) {
+		this.study = study;
 	}
 	public void setPractice(ExadelPractice practice) {
 		this.practice = practice;
 		this.practice.setStudent(this);
 	}
 	
-	@OneToOne(cascade = CascadeType.ALL, optional=true,orphanRemoval=true)
-	@JoinColumn (name="id", nullable=false)
-	public ExadelWork getWork() {
-		return work;
-	}
 	public void setWork(ExadelWork work) {
 		this.work = work;
 		this.work.setStudent(this);
 	}
-
+	public void setSkillSet(Set<Skill> skillSet) {
+		this.skillSet = skillSet;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void setSkype(String skype) {
+		this.skype = skype;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
 }
