5400fe23fce6723b80395bf2275b1c6c23666b9b
IPoyda <Rock.Ivan.Poyda@gmail.com>
2014-07-23 14:30:09 +0300

    hibernate.cfg.xml added StudentLog mapping

--- src/main/java/com/exadel/controller/RestURIConstants.java
+++ src/main/java/com/exadel/controller/RestURIConstants.java
@@ -4,6 +4,6 @@ public class RestURIConstants {
     public static final String DUMMY_STUDENT = "/rest/stud/dummy";
     public static final String GET_STUDENT = "/rest/stud/{id}";
     public static final String GET_ALL_STUDENT = "/rest/studs";
-    //public static final String CREATE_EMP = "/rest/emp/create";
+    public static final String CREATE_STUDENT = "/rest/stud/create";
     //public static final String DELETE_EMP = "/rest/emp/delete/{id}";
 }
