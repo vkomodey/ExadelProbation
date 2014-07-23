1ee57acc924277328caaeff26586fc097f62c5ed
IPoyda <Rock.Ivan.Poyda@gmail.com>
2014-07-23 14:29:48 +0300

    hibernate.cfg.xml added StudentLog mapping

---
+++ src/main/java/com/exadel/controller/RegistrationController.java
@@ -0,0 +1,35 @@
+package com.exadel.controller;
+
+
+import com.exadel.model.entity.Student;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@Controller
+public class RegistrationController {
+
+    public static Logger logger= LoggerFactory.getLogger(RegistrationController.class);
+
+    @RequestMapping(value = RestURIConstants.CREATE_STUDENT, method = RequestMethod.POST)
+    public @ResponseBody void getJsonData(@RequestBody String str) {
+        logger.info("Start createStudent.");
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            Student student =  mapper.readValue(str,Student.class);
+            logger.info(student.toString());
+            student.setPassword("11111");
+            System.out.println(student.getLogin());
+            System.out.println(student.getPassword());
+
+            // дальше типа сохраняем
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
