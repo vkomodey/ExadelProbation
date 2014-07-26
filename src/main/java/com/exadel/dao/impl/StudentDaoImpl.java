df2d95cd620e7bcdb229eec64f55c8d176f59c48
Dennis Borisevich <elfmax@tut.by>
2014-07-27 01:40:52 +0300

    implemented find by login in StudentDaoImpl

--- src/main/java/com/exadel/dao/impl/StudentDaoImpl.java
+++ src/main/java/com/exadel/dao/impl/StudentDaoImpl.java
@@ -1,13 +1,23 @@
 package com.exadel.dao.impl;
 
+import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 
 import com.exadel.dao.StudentDao;
 import com.exadel.model.entity.student.Student;
 
 @Repository
-public class StudentDaoImpl extends GenericLivingDaoImpl<Student> implements StudentDao {
-    public Student find(long id) {
-        return (Student) getSessionFactory().getCurrentSession().get(Student.class, id);
-    }
+public class StudentDaoImpl extends GenericLivingDaoImpl<Student> implements
+		StudentDao {
+	public Student find(long id) {
+		return (Student) getSessionFactory().getCurrentSession().get(
+				Student.class, id);
+	}
+
+	public Student find(String login) {
+		Session session = getSessionFactory().getCurrentSession();
+		Student stud = (Student) session.bySimpleNaturalId(Student.class).load(
+				"login");
+		return stud;
+	}
 }
