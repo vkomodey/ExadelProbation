e55ff811538312ed1e97b675ea67af88be8bcf94
Dennis Borisevich <elfmax@tut.by>
2014-07-27 02:01:37 +0300

    added @Repository on FeedbackDaoImpl to be recognized by spring

--- src/main/java/com/exadel/dao/impl/FeedbackDaoImpl.java
+++ src/main/java/com/exadel/dao/impl/FeedbackDaoImpl.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.springframework.stereotype.Repository;
 
 import com.exadel.dao.FeedbackDao;
 import com.exadel.model.entity.Feedback;
 import com.exadel.model.entity.student.Student;
-
+@Repository
 public class FeedbackDaoImpl extends GenericDaoImpl<Feedback> implements
 		FeedbackDao {
 	
