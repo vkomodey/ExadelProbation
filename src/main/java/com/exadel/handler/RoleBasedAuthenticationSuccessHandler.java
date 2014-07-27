66f1cfc30582c45c7cda3b8f39c2b8e6bcb54d81
garethspurs <garethspurs95@gmail.com>
2014-07-27 04:01:46 +0300

    role based authorization

---
+++ src/main/java/com/exadel/handler/RoleBasedAuthenticationSuccessHandler.java
@@ -0,0 +1,30 @@
+package com.exadel.handler;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+public class RoleBasedAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    private Map<String, String> roleUrlMap;
+
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+
+        if (authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            String role = userDetails.getAuthorities().isEmpty() ? null : userDetails.getAuthorities().toArray()[0].toString();
+            response.sendRedirect(request.getContextPath() + roleUrlMap.get(role));
+        }
+    }
+
+    public void setRoleUrlMap(Map<String, String> roleUrlMap) {
+        this.roleUrlMap = roleUrlMap;
+    }
+}
