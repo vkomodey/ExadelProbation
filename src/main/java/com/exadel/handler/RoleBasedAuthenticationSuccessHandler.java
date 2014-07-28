b850b97efa3d0e00935db2fc00c3e289e654fd83
Dennis Borisevich <elfmax@tut.by>
2014-07-28 11:48:05 +0300

    fixed handler recognition, added binary database backup

--- src/main/java/com/exadel/handler/RoleBasedAuthenticationSuccessHandler.java
+++ src/main/java/com/exadel/handler/RoleBasedAuthenticationSuccessHandler.java
@@ -3,13 +3,15 @@ package com.exadel.handler;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Service;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.util.Map;
-
+@Service
 public class RoleBasedAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
     private Map<String, String> roleUrlMap;
 
