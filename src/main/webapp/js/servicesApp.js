250df8a62fccbe3f5c29b442c6e152a34b42e4af
Administrator <belskiyvv@gmail.com>
2014-07-28 11:42:44 +0300

    sheet

--- src/main/webapp/js/servicesApp.js
+++ src/main/webapp/js/servicesApp.js
@@ -1,7 +1,7 @@
 studentsServices = angular.module('studentsServices',['ngResource']);
 
 studentsServices.factory('feedbacksList',['$resource','$routeParams', function($resource,$routeParams) {
-    return $resource('rest/stud/:studId', {}, {
+    return $resource('/rest/stud/:studId', {}, {
         getStudentsList: {method: 'GET', params: {studId: 'all'}, isArray: true},
         getFeedbacksList: {method: 'GET', isArray: true}
     });
