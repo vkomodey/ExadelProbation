e7cae02de3f3db606feba56767c67559371c1f0f
Dennis Borisevich <elfmax@tut.by>
2014-08-06 14:47:25 +0300

    added @Service to FilterServiceImpl

--- src/main/java/com/exadel/service/impl/FilterServiceImpl.java
+++ src/main/java/com/exadel/service/impl/FilterServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.exadel.dao.CuratorDao;
 import com.exadel.dao.StudentDao;
@@ -12,16 +14,17 @@ import com.exadel.model.entity.User;
 import com.exadel.model.entity.student.Student;
 import com.exadel.model.entity.student.Technology;
 import com.exadel.service.FilterService;
-
+@Service
 public class FilterServiceImpl implements FilterService {
 	@Autowired
 	StudentDao studentDao;
 	@Autowired
 	CuratorDao curatorDao;
+	@Transactional
 	public List<String> getAllUniversities() {
 		return studentDao.getUniversities();
 	}
-
+	@Transactional
 	public Set<Technology> getAllCurrentUsedTechnologies() {
 		List<Student> list=studentDao.getAll();
 		Set<Technology> technologies=new HashSet<>();
@@ -30,16 +33,16 @@ public class FilterServiceImpl implements FilterService {
 		}
 		return technologies;
 	}
-
+	@Transactional
 	public List<String> getAllFaculties() {
 		
 		return studentDao.getFaculties();
 	}
-
+	@Transactional
 	public List<Long> getAllStudyEndYears() {
 		return studentDao.getStudyEndYears();
 	}
-
+	@Transactional
 	public Set<User> getAllUsedCurators() {
 		return new HashSet<User>(curatorDao.getActive());
 	}
