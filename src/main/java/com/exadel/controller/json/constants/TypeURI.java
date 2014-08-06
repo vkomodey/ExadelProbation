85c2d4a43c491829bffac682bb28739c9f2eed1b
Dennis Borisevich <elfmax@tut.by>
2014-08-06 12:17:50 +0300

    types for filter

--- src/main/java/com/exadel/controller/json/constants/TypeURI.java
+++ src/main/java/com/exadel/controller/json/constants/TypeURI.java
@@ -3,5 +3,5 @@ package com.exadel.controller.json.constants;
 public class TypeURI {
 
 	public static final String GET_ALL_SKILLTYPE = "types/skill/get";
-
+	public static final String GET_ALL_TECHNOLOGIES = "types/technology/get";
 }
