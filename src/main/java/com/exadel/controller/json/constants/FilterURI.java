3a7e69f45fbb1690565698bdce93f8c8f5c84ab6
Dennis Borisevich <elfmax@tut.by>
2014-08-06 14:46:59 +0300

    fixed curator filter URI

--- src/main/java/com/exadel/controller/json/constants/FilterURI.java
+++ src/main/java/com/exadel/controller/json/constants/FilterURI.java
@@ -6,6 +6,6 @@ public class FilterURI {
 	public static final String GET_ALL_FACULTIES = "filter/faculty";
 	public static final String GET_ALL_STUDY_END_YEARS = "filter/study_end_year";
 	public static final String GET_ALL_USED_CURATORS = "filter/used_curator";
-	public static final String GET_ALL_CURRENT_USED_TECHNOLOGIES = "filter/used_curator";
+	public static final String GET_ALL_CURRENT_USED_TECHNOLOGIES = "filter/current_used_tech";
 
 }
