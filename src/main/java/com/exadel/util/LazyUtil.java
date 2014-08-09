dda88cd8f60c130edfc63914285299079297fbc4
Dennis Borisevich <elfmax@tut.by>
2014-08-09 15:43:38 +0300

    project lazytouch

--- src/main/java/com/exadel/util/LazyUtil.java
+++ src/main/java/com/exadel/util/LazyUtil.java
@@ -1,6 +1,8 @@
 package com.exadel.util;
 
+import com.exadel.model.entity.Project;
 import com.exadel.model.entity.student.Student;
+import com.exadel.model.entity.student.Technology;
 
 public class LazyUtil {
 
@@ -16,6 +18,15 @@ public class LazyUtil {
 	public static void lazyTouch(Student student){
 	    student.getStudy().getExams().size();
 	    student.getSkillSet().size();
+	}
+
+	public static void lazyTouchProject(Project project){
+	    for(Student student:project.getStudents()){
+	        student.getId();
+	    }
+	    for(Technology technology:project.getUsedTechnologies()){
+	        technology.getId();
+	    }
 	}
 
 }
