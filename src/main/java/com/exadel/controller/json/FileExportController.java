4287ee23861799d8769dc55640dbce8078e0b8a9
Dennis Borisevich <elfmax@tut.by>
2014-08-10 14:36:45 +0300

    simple two-phase return, without suitable filename.

--- src/main/java/com/exadel/controller/json/FileExportController.java
+++ src/main/java/com/exadel/controller/json/FileExportController.java
@@ -3,7 +3,10 @@ package com.exadel.controller.json;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import com.exadel.controller.json.constants.ExportURI;
+import com.exadel.model.entity.student.Student;
 import com.exadel.service.StudentService;
 import com.exadel.util.JsonUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -18,24 +21,49 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class FileExportController {
 
-    private static Logger logger = LoggerFactory
+    private static final String EXCELLIST_ATTRIBUTE = "excellist";
+	private static final String PDFLIST_ATTRIBUTE = "pdflist";
+	private static Logger logger = LoggerFactory
             .getLogger(FileExportController.class);
     @Autowired
     StudentService service;
 
     @RequestMapping(value = ExportURI.DOWNLOAD_EXCEL, method = RequestMethod.GET)
-    public ModelAndView downloadExcel(@RequestParam("ids")  String str,ObjectMapper mapper) throws IOException {
+    public ModelAndView getPreparedExcel(HttpSession session) throws IOException {
         logger.info("Getting Excel file");
-        List<Long> list=JsonUtil.readBelskiyIdObject(str);
+        //List<Long> list=JsonUtil.readBelskiyIdObject(str);
         //List<Long> list=mapper.readValue(str, JsonUtil.listOfLongTypeRef);
-        return new ModelAndView("excelView", "list", service.getAll(list));
+        @SuppressWarnings("unchecked")
+		List<Long> list=(List<Long>) session.getAttribute(EXCELLIST_ATTRIBUTE);
+        List<Student> students=service.getAll(list);
+        session.removeAttribute(EXCELLIST_ATTRIBUTE);
+        return new ModelAndView("excelView", "list", students);
     }
-
+    
     @RequestMapping(value = ExportURI.DOWNLOAD_PDF, method = RequestMethod.GET)
-    public ModelAndView downloadPDF(@RequestParam("ids") String str,ObjectMapper mapper) throws IOException {
-        logger.info("Getting pdf file");
-        List<Long> list=JsonUtil.readBelskiyIdObject(str);
-        //List<Long> list=mapper.readValue(str, JsonUtil.listOfLongTypeRef);
-        return new ModelAndView("pdfView", "list", service.getAll(list));
+    public ModelAndView getPreparedPDF(HttpSession session) throws IOException {
+        logger.info("Getting PDF file");
+        @SuppressWarnings("unchecked")
+		List<Long> list=(List<Long>) session.getAttribute(PDFLIST_ATTRIBUTE);
+        List<Student> students=service.getAll(list);
+        session.removeAttribute(PDFLIST_ATTRIBUTE);
+        return new ModelAndView("pdfView", "list", students);
+    }
+    @RequestMapping(value = ExportURI.DOWNLOAD_EXCEL, method = RequestMethod.POST)
+    public @ResponseBody void prepareExcel(@RequestBody  String str,ObjectMapper mapper,HttpSession session) throws IOException {
+        logger.info("preparing studlist for Excel file");
+        //List<Long> list=JsonUtil.readBelskiyIdObject(str);
+        List<Long> list=mapper.readValue(str, JsonUtil.listOfLongTypeRef);
+        session.setAttribute(EXCELLIST_ATTRIBUTE, list);
+        //return new ModelAndView("excelView", "list", service.getAll(list));
+        
+    }
+    @RequestMapping(value = ExportURI.DOWNLOAD_PDF, method = RequestMethod.POST)
+    public @ResponseBody void preparePDF(@RequestBody String str,ObjectMapper mapper,HttpSession session) throws IOException {
+        logger.info("preparing studlist for PDF file");
+        //List<Long> list=JsonUtil.readBelskiyIdObject(str);
+        List<Long> list=mapper.readValue(str, JsonUtil.listOfLongTypeRef);
+        session.setAttribute(PDFLIST_ATTRIBUTE, list);
+        //return new ModelAndView("pdfView", "list", service.getAll(list));
     }
 }
