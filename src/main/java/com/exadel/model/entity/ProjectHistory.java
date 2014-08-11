7244d97db44ae15f72d84e94c95680bafdcb49e9
garethspurs <garethspurs95@gmail.com>
2014-08-11 23:09:18 +0300

    add project_history entity

---
+++ src/main/java/com/exadel/model/entity/ProjectHistory.java
@@ -0,0 +1,41 @@
+package com.exadel.model.entity;
+
+import com.exadel.model.entity.student.Student;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="project_history")
+public class ProjectHistory {
+    private long id;
+    private String title;
+    private Student student;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @OneToOne
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    @Column(name = "title", nullable = false)
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
