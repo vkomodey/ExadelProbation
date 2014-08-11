8fe4ef79405a1e4dd26a18ca53b048fe1ee53b57
Administrator <belskiyvv@gmail.com>
2014-08-11 19:00:34 +0300

    add role "feedbacked", by belskiy

--- src/main/webapp/js/controllers/MakeRoleCtrl.js
+++ src/main/webapp/js/controllers/MakeRoleCtrl.js
@@ -4,7 +4,7 @@
 studentsControllers.controller('MakeRoleCtrl', ['$scope','$http','$q', function($scope, $http,$q) {
     var deferred = $q.defer();
     $http.get('/rest/me/role').success(function(data){
-        if(data=='ROLE_ADMIN') {
+        if(data=='ROLE_ADMIN' || data=='ROLE_FEEDBACKER') {
             $scope.link_ProjectList = true;
             $scope.link_EmployeeList = true;
             $scope.link_Pdf_Excel = true;
