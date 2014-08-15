f81f7bc4605c996bda300bb4e0399bff13f2cc67
Administrator <belskiyvv@gmail.com>
2014-08-15 10:24:51 +0300

    cancel last changes,by belskiy

--- src/main/webapp/js/built.js
+++ src/main/webapp/js/built.js
@@ -953,7 +953,6 @@ studentsControllers.controller('MakeRoleCtrl', ['$scope','$http', function($scop
                 $scope.disableAddFeedback = true;
                 $scope.link_saveStudentInfo = true;
                 $scope.link_projectHistory = true;
-                $scope.disableFeedbackEdit = true;
                 break;
             case "ROLE_CURATOR":
             case "ROLE_FEEDBACKER":
